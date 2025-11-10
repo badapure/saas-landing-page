ef1d8daec4a0cbc497b7c398d06f01b1b2c1ebf9
Harsh Badapure <harshbadapure99@gmail.com>
2025-11-10 13:32:58 +0530

    Update index.jsx

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -129,7 +129,7 @@ export const features = [
       id: "1",
       title: "Overdrive",
       priceMonthly: 79,
-      priceYearly: 79,
+      priceYearly: 85,
       caption: "Most popular plan",
       features: [
         "All Starter features",
